68e1940cecb17de81358862f0c0a2eb674a20af8
Doug DeJong <dougdejong@gmail.com>
2018-09-28 15:34:42 -0400

    fully functinal

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -28,20 +28,49 @@ class ProjectDetails extends Component {
     })
   }
 
- 
+  
+  
+  
   renderEditForm = () => {
     if(!this.state.title){
       this.getSingleProject();
     } else {
-    //                                                    {...props} => so we can have 'this.props.history' in Edit.js
-    //                                                                                          ^
-    //                                                                                          |
+      //                                                    {...props} => so we can have 'this.props.history' in Edit.js
+      //                                                                                          ^
+      //                                                                                          |
       return <div>
         <EditProject theProject={this.state} getTheProject={this.getSingleProject} {...this.props} />
              <AddTask theProject={this.state} getTheProject={this.getSingleProject}/>
              </div>
     }
   }
+  
+  showTasks = () => {
+    if(!this.state.title){
+      this.getSingleProject();
+    } else {
+    console.log(this.state)
+   return this.state.tasks.map((task, index) => {
+    return (
+      <div key={task._id}>
+      
+      <h3>{task.title}</h3>
+      
+      <p style={{maxWidth: '400px'}} >{task.description} </p>
+      <button onClick={() => this.deleteTask(task._id)}>Delete Task</button>
+      </div>
+      )})
+  }
+}
+deleteTask = (id) => {
+  axios.delete(`http://localhost:5000/api/tasks/${id}`)
+  .then( responseFromApi =>{
+      this.getSingleProject();   
+  })
+  .catch((err)=>{
+      console.log(err)
+  })
+}
 
 // DELETE PROJECT:
   deleteProject = (id) => {
@@ -60,8 +89,10 @@ class ProjectDetails extends Component {
       <div>
         <h1>{this.state.title}</h1>
         <p>{this.state.description}</p>
-        <div>{this.renderEditForm()} </div>
-        <button onClick={() => this.deleteProject(this.state._id)}>Delete project</button>
+          <div>{this.renderEditForm()} </div>
+        <div>{this.showTasks()}
+          </div>
+          <button onClick={() => this.deleteProject(this.state._id)}>Delete project</button>
         <Link to={'/projects'}>Back to projects</Link>
       </div>
     )
